996bdf0c96fc33b2a9f7111a44c83169243cd618
管晓敏 <gxm@guanxiaomins-MacBook-Pro.local>
2020-02-11 17:38:24 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.ts',
+  module: {
+    rules: [
+      { test: /\.ts$/, loader: 'ts-loader', exclude: '/node_modules/' },
+      { test: /phaser\.js$/, loader: 'expose-loader?Phaser' },
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+  },
+  // devServer: {
+  //   contentBase: path.resolve(__dirname, './dist'),
+  //   publicPath: '/dist',
+  //   host: '0.0.0.0',
+  //   port: 8082,
+  //   open: false
+  // }
+
+  devServer: {
+    contentBase: path.resolve(__dirname, './'),
+    publicPath: '/dist/',
+    host: '0.0.0.0',
+    port: 8082,
+    watchOptions: {
+      poll: 1000
+    },
+    open: false
+  },
+};
