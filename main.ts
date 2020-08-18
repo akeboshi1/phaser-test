bdc06456232c6f63ed9e0134a13ab0ae3a857791
guanxiaomin <guanxiaomin@apowo.com>
2020-08-18 09:25:51 +0800

    update workercontrol

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 import "phaser";
-import TestWorker from "worker-loader?name=dist/[name].js!./TestWorker";
-import { Base64, decode, encode } from "js-base64";
+// import TestWorker from "worker-loader?name=dist/[name].js!./TestWorker";
+// import { Base64, decode, encode } from "js-base64";
+import { WorkerControl } from "./src/workerControl";
+import { Inject } from "common-injector";
 export class World {
     constructor() {
         const config = {
@@ -19,7 +21,7 @@ export class World {
 export class GameScene extends Phaser.Scene {
     private graphics: Phaser.GameObjects.Graphics;
     private rt: Phaser.GameObjects.RenderTexture;
-    private worker: TestWorker;
+    @Inject() private workerControl: WorkerControl;
     constructor() {
         super({ key: GameScene.name });
     }
@@ -29,16 +31,16 @@ export class GameScene extends Phaser.Scene {
         this.graphics = this.add.graphics();
         this.graphics.setVisible(false);
 
-        this.drawStar(this.graphics, 200, 200, 5, 200, 100, 0xffff00);
-
         this.rt = this.add.renderTexture(400, 300, 400, 400).setOrigin(0.5);
-        this.worker = new TestWorker();
-        const self = this;
-        this.worker.onmessage = (event: any) => {
-            self.onWorkerMessage(event.data);
-        };
-        // this.worker.postMessage({ "method": "start", "url": "https://a.tooqing.com/m/resources/ui/baseView/mainui_mobile.png" });
-        this.worker.postMessage({ "method": "start", "url": "https://a.tooqing.com/m/resources/ui/baseView/mainui_mobile.json" });
+        this.workerControl = new WorkerControl();
+        this.workerControl.startHandler();
+        // this.worker = new TestWorker();
+        // const self = this;
+        // this.worker.onmessage = (event: any) => {
+        //     self.onWorkerMessage(event.data);
+        // };
+        // // this.worker.postMessage({ "method": "start", "url": "https://a.tooqing.com/m/resources/ui/baseView/mainui_mobile.png" });
+        // this.worker.postMessage({ "method": "start", "url": "https://a.tooqing.com/m/resources/ui/baseView/mainui_mobile.json" });
     }
 
     public update() {
@@ -49,98 +51,6 @@ export class GameScene extends Phaser.Scene {
         // this.rt.draw(this.graphics);
     }
 
-    private onWorkerMessage(data: any) {
-        const self = this;
-        const method = data.method;
-        switch (method) {
-            case "completeHandler":
-                // const reader = new FileReader();
-                // reader.addEventListener("loadend", function () {
-                    // reader.result 包含被转化为类型数组 typed array 的 blob
-                    // tslint:disable-next-line:no-console
-                //     console.log(reader.result);
-                //     const url: string = String(reader.result);
-                //     self.textures.on("addtexture", () => {
-                //         const img = self.make.image(undefined, true);
-                //         img.setTexture("logo");
-                //         img.x = 100;
-                //         img.y = 100;
-                //     }, this);
-                //     self.textures.addBase64("logo", url);
-                    // canvas 方式
-                    // document.getElementById("testImg")
-                    //     .setAttribute(
-                    //         "src", String(reader.result)
-                    //     );
-                // });
-                const arr = [];
-                arr.push(data.data);
-
-                // 由于会产生异步所以使用promise来限制回调
-                const p = new Promise(function (resolve, reject) {
-                    const arr2 = [];
-                    arr.forEach((val, index) => {
-                        const reader = new FileReader();
-                        reader.readAsDataURL(val);
-                        reader.onload = function (e) {
-                            // tslint:disable-next-line:no-console
-                            console.log(e);
-                            const resList = String(e.target.result).split(",");
-                            resolve(resList[1]);
-                            // arr2.push(e.target.result);
-                            // // 当arr数组的元素个数与arr2数组的元素个数相等时证明已经全部转换完成
-                            // if (arr2.length === arr.length) {
-                            //     resolve(arr2);
-                            // }
-                        }
-                    })
-                })
-
-                // base64转换完成时执行，将整个res（arr2）转换为JSON字符串
-                p.then(function (res) {
-                    const tmp = Base64.decode(String(res));
-                    // self.cache.json.add("logo", JSON.parse(tmp));
-                    // tslint:disable-next-line:no-console
-                    console.log(JSON.parse(tmp));
-                }, function (error) {
-
-                })
-
-                // reader.readAsDataURL(data.data);
-                const objectURL = URL.createObjectURL(data.data);
-                // tslint:disable-next-line:no-console
-                console.log(objectURL);
-                this.load.image("logo", objectURL);
-                break;
-        }
-    }
-
-    private drawStar(graphics, cx, cy, spikes, outerRadius, innerRadius, color) {
-        let rot = Math.PI / 2 * 3;
-        let x = cx;
-        let y = cy;
-        const step = Math.PI / spikes;
-
-        graphics.fillStyle(color, 1.0);
-        graphics.beginPath();
-        graphics.moveTo(cx, cy - outerRadius);
-
-        for (let i = 0; i < spikes; i++) {
-            x = cx + Math.cos(rot) * outerRadius;
-            y = cy + Math.sin(rot) * outerRadius;
-            graphics.lineTo(x, y);
-            rot += step;
-
-            x = cx + Math.cos(rot) * innerRadius;
-            y = cy + Math.sin(rot) * innerRadius;
-            graphics.lineTo(x, y);
-            rot += step;
-        }
-
-        graphics.lineTo(cx, cy - outerRadius);
-        graphics.closePath();
-        graphics.fillPath();
-    }
 }
 
 window.onload = () => {
---
+++ src/workerControl.ts
@@ -0,0 +1,237 @@
+import TestWorker from "worker-loader?name=dist/[name].js!../TestWorker";
+import { Base64, decode, encode } from "js-base64";
+import "common-injector";
+import { Injectable, Inject, rootInjector } from "common-injector";
+// todo rpc module
+export class WorkerControl {
+    public worker: TestWorker;
+    constructor() {
+        this.worker = new TestWorker();
+        const self = this;
+        this.worker.onmessage = (event: any) => {
+            self.onWorkerMessage(event.data);
+        };
+    }
+
+    public startHandler() {
+        // this.worker.postMessage({ "method": "start", "url": "https://a.tooqing.com/m/resources/ui/baseView/mainui_mobile.png" });
+        this.worker.postMessage({ "method": "start", "url": "https://a.tooqing.com/m/resources/ui/baseView/mainui_mobile.json" });
+    }
+
+    public stopHandler() {
+        this.worker.postMessage({ "method": "stop" });
+    }
+
+    public terminate() {
+        this.worker.terminate();
+    }
+    public completeHandler(data) {
+        // 由于会产生异步所以使用promise来限制回调
+        const p = new Promise(function (resolve, reject) {
+            const arr = [];
+            arr.push(data.data);
+            arr.forEach((val, index) => {
+                const reader = new FileReader();
+                reader.readAsDataURL(val);
+                reader.onload = function (e) {
+                    // tslint:disable-next-line:no-console
+                    console.log(e);
+                    const resList = String(e.target.result).split(",");
+                    resolve(resList[1]);
+                }
+            })
+        })
+
+        // base64转换完成时执行，将整个res（arr2）转换为JSON字符串
+        p.then(function (res) {
+            const tmp = Base64.decode(String(res));
+            // self.cache.json.add("logo", JSON.parse(tmp));
+            // tslint:disable-next-line:no-console
+            console.log(JSON.parse(tmp));
+        }, function (error) {
+
+        })
+
+        // reader.readAsDataURL(data.data);
+        const objectURL = URL.createObjectURL(data.data);
+        // tslint:disable-next-line:no-console
+        console.log(objectURL);
+    }
+    private onWorkerMessage(data: any) {
+        const self = this;
+        const method = data.method;
+        switch (method) {
+            case "completeHandler":
+                // const reader = new FileReader();
+                // reader.addEventListener("loadend", function () {
+                // reader.result 包含被转化为类型数组 typed array 的 blob
+                // tslint:disable-next-line:no-console
+                //     console.log(reader.result);
+                //     const url: string = String(reader.result);
+                //     self.textures.on("addtexture", () => {
+                //         const img = self.make.image(undefined, true);
+                //         img.setTexture("logo");
+                //         img.x = 100;
+                //         img.y = 100;
+                //     }, this);
+                //     self.textures.addBase64("logo", url);
+                // canvas 方式
+                // document.getElementById("testImg")
+                //     .setAttribute(
+                //         "src", String(reader.result)
+                //     );
+                // });
+                const arr = [];
+                arr.push(data.data);
+
+                // 由于会产生异步所以使用promise来限制回调
+                const p = new Promise(function (resolve, reject) {
+                    const arr2 = [];
+                    arr.forEach((val, index) => {
+                        const reader = new FileReader();
+                        reader.readAsDataURL(val);
+                        reader.onload = function (e) {
+                            // tslint:disable-next-line:no-console
+                            console.log(e);
+                            const resList = String(e.target.result).split(",");
+                            resolve(resList[1]);
+                        }
+                    })
+                })
+
+                // base64转换完成时执行，将整个res（arr2）转换为JSON字符串
+                p.then(function (res) {
+                    const tmp = Base64.decode(String(res));
+                    // self.cache.json.add("logo", JSON.parse(tmp));
+                    // tslint:disable-next-line:no-console
+                    console.log(JSON.parse(tmp));
+                }, function (error) {
+
+                })
+
+                // reader.readAsDataURL(data.data);
+                const objectURL = URL.createObjectURL(data.data);
+                // tslint:disable-next-line:no-console
+                console.log(objectURL);
+                // this.load.image("logo", objectURL);
+                break;
+        }
+    }
+}
+// @Injectable({ provide: WorkerControl })
+// export class DemoControl {
+//     public worker: TestWorker;
+//     constructor() {
+//         this.worker = new TestWorker();
+//         const self = this;
+//         this.worker.onmessage = (event: any) => {
+//             self.onWorkerMessage(event.data);
+//         };
+//     }
+
+//     @Inject() public startHandler() {
+//         // this.worker.postMessage({ "method": "start", "url": "https://a.tooqing.com/m/resources/ui/baseView/mainui_mobile.png" });
+//         this.worker.postMessage({ "method": "start", "url": "https://a.tooqing.com/m/resources/ui/baseView/mainui_mobile.json" });
+//     }
+
+//     @Inject() public stopHandler() {
+//         this.worker.postMessage({ "method": "stop" });
+//     }
+
+//     @Inject() public terminate() {
+//         this.worker.terminate();
+//     }
+//     @Inject() public completeHandler(data) {
+//         // 由于会产生异步所以使用promise来限制回调
+//         const p = new Promise(function (resolve, reject) {
+//             const arr = [];
+//             arr.push(data.data);
+//             arr.forEach((val, index) => {
+//                 const reader = new FileReader();
+//                 reader.readAsDataURL(val);
+//                 reader.onload = function (e) {
+//                     // tslint:disable-next-line:no-console
+//                     console.log(e);
+//                     const resList = String(e.target.result).split(",");
+//                     resolve(resList[1]);
+//                 }
+//             })
+//         })
+
+//         // base64转换完成时执行，将整个res（arr2）转换为JSON字符串
+//         p.then(function (res) {
+//             const tmp = Base64.decode(String(res));
+//             // self.cache.json.add("logo", JSON.parse(tmp));
+//             // tslint:disable-next-line:no-console
+//             console.log(JSON.parse(tmp));
+//         }, function (error) {
+
+//         })
+
+//         // reader.readAsDataURL(data.data);
+//         const objectURL = URL.createObjectURL(data.data);
+//         // tslint:disable-next-line:no-console
+//         console.log(objectURL);
+//     }
+//     private onWorkerMessage(data: any) {
+//         const self = this;
+//         const method = data.method;
+//         this.[method].apply(data.data);
+//         switch (method) {
+//             case "completeHandler":
+//                 // const reader = new FileReader();
+//                 // reader.addEventListener("loadend", function () {
+//                 // reader.result 包含被转化为类型数组 typed array 的 blob
+//                 // tslint:disable-next-line:no-console
+//                 //     console.log(reader.result);
+//                 //     const url: string = String(reader.result);
+//                 //     self.textures.on("addtexture", () => {
+//                 //         const img = self.make.image(undefined, true);
+//                 //         img.setTexture("logo");
+//                 //         img.x = 100;
+//                 //         img.y = 100;
+//                 //     }, this);
+//                 //     self.textures.addBase64("logo", url);
+//                 // canvas 方式
+//                 // document.getElementById("testImg")
+//                 //     .setAttribute(
+//                 //         "src", String(reader.result)
+//                 //     );
+//                 // });
+//                 const arr = [];
+//                 arr.push(data.data);
+
+//                 // 由于会产生异步所以使用promise来限制回调
+//                 const p = new Promise(function (resolve, reject) {
+//                     const arr2 = [];
+//                     arr.forEach((val, index) => {
+//                         const reader = new FileReader();
+//                         reader.readAsDataURL(val);
+//                         reader.onload = function (e) {
+//                             // tslint:disable-next-line:no-console
+//                             console.log(e);
+//                             const resList = String(e.target.result).split(",");
+//                             resolve(resList[1]);
+//                         }
+//                     })
+//                 })
+
+//                 // base64转换完成时执行，将整个res（arr2）转换为JSON字符串
+//                 p.then(function (res) {
+//                     const tmp = Base64.decode(String(res));
+//                     // self.cache.json.add("logo", JSON.parse(tmp));
+//                     // tslint:disable-next-line:no-console
+//                     console.log(JSON.parse(tmp));
+//                 }, function (error) {
+
+//                 })
+
+//                 // reader.readAsDataURL(data.data);
+//                 const objectURL = URL.createObjectURL(data.data);
+//                 // tslint:disable-next-line:no-console
+//                 console.log(objectURL);
+//                 // this.load.image("logo", objectURL);
+//                 break;
+//         }
+//     }
+// }
